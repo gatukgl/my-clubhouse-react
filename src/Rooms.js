7ed8124517d552b47591804bfbee0b92a4278f93
Gatuk Chattanon <gatukgl@gmail.com>
2021-03-28 18:56:43 +0700

    Refactor get rooms from api

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,44 +1,15 @@
-import axios from 'axios'
 import React from 'react'
+
+import { getRooms } from './apis'
 import { Room } from './Room'
 
 export class Rooms extends React.Component {
   state = { rooms: [] }
 
-  mapRoomsFromApi(data) {
-    const rooms = data.map((item) => {
-      return {
-        id: item.id,
-        topic: item.topic,
-        listenerCount: item.room_listener.length,
-        moderators: [
-          {
-            username: item.room_moderator[0].user.username,
-            name: item.room_moderator[0].user.name,
-            imageUrl: item.room_moderator[0].user.profile_picture,
-          },
-          {
-            username: item.room_moderator[1].user.username,
-            name: item.room_moderator[1].user.name,
-            imageUrl: item.room_moderator[1].user.profile_picture,
-          },
-        ],
-      }
-    })
-
-    return rooms
-  }
-
   componentDidMount() {
-    const url = 'http://192.168.1.108:8000/api/rooms/'
-    axios
-      .get(url)
-      .then((response) => {
-        const data = response.data
-        const rooms = this.mapRoomsFromApi(data)
-        this.setState({ rooms: rooms })
-      })
-      .catch((error) => console.error('[Get rooms]', error))
+    getRooms().then((rooms) => {
+      this.setState({ rooms: rooms })
+    })
   }
 
   render() {
---
+++ src/apis.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://192.168.1.108:80/api/',
+})
+
+const mapRoomsFromApi = (data) => {
+  const rooms = data.map((item) => {
+    return {
+      id: item.id,
+      topic: item.topic,
+      listenerCount: item.room_listener.length,
+      moderators: [
+        {
+          username: item.room_moderator[0].user.username,
+          name: item.room_moderator[0].user.name,
+          imageUrl: item.room_moderator[0].user.profile_picture,
+        },
+        {
+          username: item.room_moderator[1].user.username,
+          name: item.room_moderator[1].user.name,
+          imageUrl: item.room_moderator[1].user.profile_picture,
+        },
+      ],
+    }
+  })
+  return rooms
+}
+
+export const getRooms = () => {
+  const url = 'rooms/'
+  return axiosInstance
+    .get(url)
+    .then((response) => {
+      const data = response.data
+      const rooms = mapRoomsFromApi(data)
+      return rooms
+    })
+    .catch((error) => console.error('[Get rooms]', error))
+}
